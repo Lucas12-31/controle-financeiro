cbfe4afc1f029830c421f269c3b045f88334114e
Lucas12-31 <lmlimoeiro@gmail.com>
2026-01-09 00:07:10 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2d8LApywbcUMORHZB0lIpcWQu-KrtHqs",
+  authDomain: "meucontrolefinanceiro-94d3a.firebaseapp.com",
+  projectId: "meucontrolefinanceiro-94d3a",
+  storageBucket: "meucontrolefinanceiro-94d3a.firebasestorage.app",
+  messagingSenderId: "592355024628",
+  appId: "1:592355024628:web:73c5d3b32902f0534288a1",
+  measurementId: "G-94T3P0XC7M"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
