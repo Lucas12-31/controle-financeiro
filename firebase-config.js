1e609136953fe3e7c205605c346b701248f59b3d
Lucas12-31 <lmlimoeiro@gmail.com>
2026-01-09 01:26:37 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js"; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyA2d8LApywbcUMORHZB0lIpcWQu-KrtHqs",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
