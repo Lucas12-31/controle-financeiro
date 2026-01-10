99d4fa0a0d56b946c3c04d8ed4e86f5207cda9b9
Lucas12-31 <lmlimoeiro@gmail.com>
2026-01-09 21:54:35 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'financeiro-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json'
+];
+
+// Instala o service worker e armazena os arquivos no cache
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Faz o app responder mesmo sem internet
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
