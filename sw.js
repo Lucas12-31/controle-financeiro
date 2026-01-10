43eddd2550bb76110f0deafb40dc8f5205059049
Lucas12-31 <lmlimoeiro@gmail.com>
2026-01-09 22:40:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const cacheName = 'financeiro-v1';
+const cacheName = 'financeiro-v2'; // Mudei para v2 para o navegador forçar a atualização
 const assets = [
   './',
   './index.html',
   './style.css',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png', // Adicione o ícone de 192px
+  './icon-512.png'  // Adicione o ícone de 512px
 ];
 
 // Instala o service worker e armazena os arquivos no cache
